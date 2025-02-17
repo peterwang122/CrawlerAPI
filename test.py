710e8e944af182619da1dcef464940386a5b8539
peterwang122 <599196959@qq.com>
2025-02-17 11:20:59 +0800

    TEST

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-import requests
-# 如果下面目标站不可用，请使用test.ipw.cn、ip.sb、ipinfo.io、ip-api.com、64.ipcheck.ing
-targetURL = "https://ipinfo.io"
-proxyAddr = "overseas.tunnel.qg.net:13871"
-authKey = "NBLHO1J5"
-password = "68E86C27232A"
-# 账密模式
-proxyUrl = "http://%(user)s:%(password)s@%(server)s" % {
-    "user": authKey,
-    "password": password,
-    "server": proxyAddr,
-}
-proxies = {
-    "http": proxyUrl,
-    "https": proxyUrl,
-}
-resp = requests.get(targetURL, proxies=proxies)
-print(resp.text)
+import time
+
+from playwright.sync_api import sync_playwright
+def get_html_content(url):
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=True)  # 使用 Firefox（或 p.chromium / p.webkit）
+        page = browser.new_page()
+        response = page.goto(url)
+        html_content = page.content()
+        timestamp = int(time.time())
+        filename = f"amazon_page_{timestamp}.html"
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(html_content)
+        print(f"✅ 页面已保存至：{filename}")
+        status_code = response.status if response else None
+        browser.close()
+        return html_content, status_code
+
+
+if __name__ == '__main__':
+    # url = "https://www.amazon.com/s?k=boxing+gym+equipment&crid=1SIJO3Q0IGNU9&sprefix=%2Caps%2C808&ref=nb_sb_ss_recent_5_0_recent"
+    url = "https://www.amazon.com/s?k=boxing+gym+equipment&page=2&xpid=72nvLEBW1cgJE&crid=1SIJO3Q0IGNU9&qid=1739533978&sprefix=%2Caps%2C808&ref=sr_pg_2"
+    html, status = get_html_content(url)
+    print(f"Status Code: {status}")
+    # print(html)
